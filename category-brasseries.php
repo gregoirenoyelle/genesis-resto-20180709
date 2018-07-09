c9bda54c7695c9b621998f3f6aaf3fdd7eaf13b1
Grégoire Noyelle <hello@gregoirenoyelle.com>
2018-07-09 15:09:33 +0200

    Appel de la fonction the_title avant le titre

--- category-brasseries.php
+++ category-brasseries.php
@@ -2,9 +2,9 @@
 
 // Fonction pour afficher du texte
 function ajouter_texte_avant_titre() {
-	echo "<p>Texte avant titre</p>";
+	the_title('<p>Mon titre est: ', '</p>');
 }
 // Hook sur genesis_entry_header pour afficher mon nouveau contenu
-add_action('genesis_entry_header', 'ajouter_texte_avant_titre', 9);
+add_action('genesis_entry_header', 'ajouter_texte_avant_titre', 12);
 
 genesis();
