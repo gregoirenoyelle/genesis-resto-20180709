7a07add47bd57d4b76f87afa2c0ad5a9d990a7a2
Grégoire Noyelle <hello@gregoirenoyelle.com>
2018-07-09 15:21:12 +0200

    Ajout d'une vidéo en haut des archives pour les brasseries.

--- category-brasseries.php
+++ category-brasseries.php
@@ -7,4 +7,10 @@ function ajouter_texte_avant_titre() {
 // Hook sur genesis_entry_header pour afficher mon nouveau contenu
 add_action('genesis_entry_header', 'ajouter_texte_avant_titre', 12);
 
+// Fonction pour ajouter une vidéo
+function ajouter_video() {
+	echo '<div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/274720181?title=0&byline=0&portrait=0" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>';
+}
+add_action('genesis_before_loop', 'ajouter_video', 9);
+
 genesis();
