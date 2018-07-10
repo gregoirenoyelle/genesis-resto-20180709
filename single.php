685e55152bb9d3f0715aaf29784602a88772b0d0
Grégoire Noyelle <hello@gregoirenoyelle.com>
2018-07-10 14:38:58 +0200

    Plusieurs manière d'afficher le champs vidéo

--- single.php
+++ single.php
@@ -2,6 +2,15 @@
 
 // Fonction pour ajouter une vidéo
 function ajouter_video() {
+
+	// Affichage de la vidéo avec echo sur la variable
+	$video = get_field('code_iframe_de_la_video');
+	echo $video;
+
+	// Affichage de la vidéo avec echo sur la fonction
+	echo get_field('code_iframe_de_la_video');
+
+	// Affichage de la vidéo avec la fonction ACF
 	the_field('code_iframe_de_la_video');
 }
 add_action('genesis_before_loop', 'ajouter_video', 9);
