5ea95b4eb51061f3adedecef582c93bd7627e57a
Grégoire Noyelle <hello@gregoirenoyelle.com>
2018-07-10 15:02:35 +0200

    Principes de base de la concaténation avec les variables et l'écriture .=

--- single-cuisinier.php
+++ single-cuisinier.php
@@ -2,4 +2,20 @@
 
 // Contenu de la fiche cuisinier
 
+function resto_contenu_cuisinier() {
+	// Variables
+	$html = 'toto';
+
+	// Affichage de la bio longue
+	$html .= 'tintin';
+
+
+	// Affichage du HTML
+	echo $html;
+
+} // FIN function resto_contenu_cuisinier()
+
+add_action('genesis_entry_content', 'resto_contenu_cuisinier');
+
+
 genesis();
