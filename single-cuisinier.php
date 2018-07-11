1799812b45f4a4b88531e5e7449c19fcf62d770a
Grégoire Noyelle <hello@gregoirenoyelle.com>
2018-07-11 16:37:29 +0200

    * Appel de la nouvelle zone de widget
    En utilisant la classe entry dans le HTML pour utiliser les même styles.

--- single-cuisinier.php
+++ single-cuisinier.php
@@ -3,5 +3,15 @@
 // Appel du Contenu de la fiche cuisinier
 get_template_part('contenu','cuisinier');
 
+// Appeler la zone de widget
+// Affichage de la zone de Widget home-top
+function resto_widget_cuisinier() {
+	genesis_widget_area( 'fiche-cuisinier', array(
+		'before' => '<div class="entry widget-area widget-cuisinier">',
+		'after'  => '</div>',
+	) );
+}
+add_action('genesis_after_entry', 'resto_widget_cuisinier' );
+
 
 genesis();
