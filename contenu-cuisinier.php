0f949e54e37f9ddd2c285da359cef50ddbc257b8
Grégoire Noyelle <hello@gregoirenoyelle.com>
2018-07-11 15:35:56 +0200

    Retirer image à la une et le contenu sur les archives de cuisinier NOTE
    Le contenu 'lire la suite....' est lié aux réglages d'archive de Genesis

--- contenu-cuisinier.php
+++ contenu-cuisinier.php
@@ -8,6 +8,11 @@ add_action( 'genesis_entry_footer', 'genesis_post_info', 12 );
 if ( ! is_singular('cuisinier') ) {
 	// Maquette Pleine largeur
 	add_filter( 'genesis_site_layout', '__genesis_return_full_width_content' );
+
+	// pour enlever l'image à la une générée automatiquement
+	remove_action( 'genesis_entry_content', 'genesis_do_post_image', 8 );
+	// pour enlever le contenu de l'éditeur (contenu classique ou extrait)
+	remove_action( 'genesis_entry_content', 'genesis_do_post_content' );	
 }
 
 
