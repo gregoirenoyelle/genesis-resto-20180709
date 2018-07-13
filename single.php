4da63523ba6ca154b69c3093addf6d5db7f954b8
Grégoire Noyelle <hello@gregoirenoyelle.com>
2018-07-13 10:58:32 +0200

    Affichage du champs Relation réglé sur IDs

--- single.php
+++ single.php
@@ -73,9 +73,47 @@ function resto_afficher_cuisinier() {
 	// Affichier le HTML
 	echo $html;
 
-}
-add_action('genesis_after_entry', 'resto_afficher_cuisinier', 9);
+} // FIN function resto_afficher_cuisinier()
+add_action('genesis_after_entry', 'resto_afficher_cuisinier', 8);
 
+
+function resto_afficher_cuisinier_ids() {
+	// Variables
+	$html = '';
+	$cuisiniers_id = get_field('selection_cuisinier_ids');
+	d($cuisiniers_id);
+
+	// article du codex: http://codex.wordpress.org/Class_Reference/WP_Query
+	$ma_boucle = new WP_Query (
+		array(
+			'post_type' => 'cuisinier',
+			'post__in' => $cuisiniers_id,
+			'orderby' => 'post__in',
+			'order' => 'ASC',
+		)
+	); // fin WP_Query 
+	// début loop $ma_boucle
+	while ( $ma_boucle->have_posts() ) : $ma_boucle->the_post(); 
+		// Variable de la boucle
+		$img_id = get_field('cuisto_photo', $id);
+		$img = wp_get_attachment_image( $img_id, 'cuisinier');
+
+		d($img_id);
+		
+		$html .= '<article class="entry">';
+			
+			$html .= sprintf( '<h3><a href="%s">%s</a></h3>', get_permalink(), get_the_title() );
+			$html .= sprintf( '<figure>%s</figure>', $img );
+
+		$html .= '</article>';
+	endwhile;
+	wp_reset_postdata();
+	// fin loop $ma_boucle	
+
+	// Afficher HTML
+	echo $html;
+} // FIN function resto_afficher_cuisinier_ids()
+add_action('genesis_after_entry', 'resto_afficher_cuisinier_ids', 9);
 
 
 genesis();
