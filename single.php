d4d5fcc2ffd17ac789dbfc1592e7ee8a611299ac
Grégoire Noyelle <hello@gregoirenoyelle.com>
2018-07-13 10:45:59 +0200

    Affichage du champs Relation réglé sur Objet Article

--- single.php
+++ single.php
@@ -33,10 +33,46 @@ add_action('genesis_after_entry', 'resto_widget_cuisinier' );
 // Afficher les cuisinier
 function resto_afficher_cuisinier() {
 	// Variables
+	$html = '';
 	$cuisiniers = get_field('selection_cuisinier');
+	$titre = get_field('resto_titre_article');
 	// d(get_fields());
 	d($cuisiniers);
+
+	// Titre 
+	if ( $titre ) {
+		$html .= sprintf('<div class="archive-description"><h3>%s</h3></div>', $titre);
+	}
+
 	
+	$html .= '<section class="selection-cuisinier">';
+
+	// Boucles de cuisinier
+
+	foreach ($cuisiniers as $cuisinier) {
+		// Variables de la boucle
+		$id = $cuisinier->ID;
+		// get_permalink avec le id car je ne suis pas dans la boucle
+		$lien = get_permalink($id);
+		// image ACF avec le id car je ne suis pas dans la boucle
+		$img_id = get_field('cuisto_photo', $id);
+		$img = wp_get_attachment_image( $img_id, 'cuisinier');
+		// d($img_id);
+		
+		$html .= '<article class="entry">';
+			$html .= sprintf( '<figure>%s</figure>', $img );
+			$html .= sprintf( '<h2><a href="%s">%s</a></h2>', $lien, $cuisinier->post_title  );
+		$html .= '</article>';
+
+	} // FIN foreach ($cuisiniers as $cuisinier)
+
+	// Fin de la boucle
+		
+	$html .= '</section>';
+
+	// Affichier le HTML
+	echo $html;
+
 }
 add_action('genesis_after_entry', 'resto_afficher_cuisinier', 9);
 
