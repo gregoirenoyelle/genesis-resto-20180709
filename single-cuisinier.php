cf65d438ec78b61979be1d59ae7aaa57e9a7aa62
Grégoire Noyelle <hello@gregoirenoyelle.com>
2018-07-13 15:58:31 +0200

    Ajout des étoiles en bas des fiches cuisinier

--- single-cuisinier.php
+++ single-cuisinier.php
@@ -13,5 +13,12 @@ function resto_widget_cuisinier() {
 }
 add_action('genesis_after_entry', 'resto_widget_cuisinier' );
 
+function resto_afficher_etoiles() {
+	// si problème mettre la globale $post
+	// global $post;
+	echo get_the_term_list($post->ID, 'etoile', '<p class="entry-meta">Nombre d\'étoiles :', ', ', '</p>' );
+}	
+add_action('genesis_entry_footer', 'resto_afficher_etoiles');
+
 
 genesis();
